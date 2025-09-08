52a105e09e5e1e4dc9ef830a342da70ff8a0972d
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2025-09-08 06:12:45 -0400

    count unique values

---
+++ count-unique-values.js
@@ -0,0 +1,23 @@
+//implement a funciont that count unique values
+//[1,1,1,1,1,2] //2
+//[1,2,3,4,4,4,7,7,12,12,13] //7
+//[] //0
+//[-2,-1,-1,0,1] //4
+
+function countUniqueValues(values) {
+    let uniqueValues = 1
+
+    if(values.length === 0)
+        return 0
+
+    for(let i = 0; i < values.length - 1; i++)
+        if(values[i+1] > values[i])
+            uniqueValues++
+
+    return uniqueValues
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2]))
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+console.log(countUniqueValues([]))
+console.log(countUniqueValues([-2,-1,-1,0,1]))
