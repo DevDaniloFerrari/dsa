e0252652d90a171ac488d4b660a4ec99733b7480
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2025-09-17 07:00:54 -0400

    improving find longes tsubstring

--- find-longest-substring.js
+++ find-longest-substring.js
@@ -1,28 +1,47 @@
-function findLongestSubstring(string) {
-  if (string.length === 0) return 0;
+// function findLongestSubstring(string) {
+//   if (string.length === 0) return 0;
 
-  let hashmap = {};
-  let longest = 0;
-  let actualSize = 0;
+//   let hashmap = {};
+//   let longest = 0;
+//   let actualSize = 0;
 
-  for (let i = 0; i < string.length; i++) {
-    if (!hashmap[string[i]]) hashmap[string[i]] = i;
-    else {
-      actualSize = Object.keys(hashmap).length;
-      if (actualSize > longest) longest = actualSize;
-      i = hashmap[string[i]];
-      hashmap = {};
-    }
+//   for (let i = 0; i < string.length; i++) {
+//     if (!hashmap[string[i]]) hashmap[string[i]] = i;
+//     else {
+//       actualSize = Object.keys(hashmap).length;
+//       if (actualSize > longest) longest = actualSize;
+//       i = hashmap[string[i]];
+//       hashmap = {};
+//     }
+//   }
+
+//   actualSize = Object.keys(hashmap).length;
+//   if (actualSize > longest) longest = actualSize;
+
+//   return longest;
+// }
+
+function findLongestSubstring(string){
+  let longest = 0
+  let seen = {}
+  let start = 0
+
+  for(let i = 0; i < string.length; i++){
+    let char = string[i]
+    
+    if(seen[char])
+      start = Math.max(start, seen[char])
+
+    longest = Math.max(longest, i - start + 1)
+
+    seen[char] = i + 1
   }
 
-  actualSize = Object.keys(hashmap).length;
-  if (actualSize > longest) longest = actualSize;
-
-  return longest;
+  return longest
 }
 
-console.log(findLongestSubstring(''))// 0
-console.log(findLongestSubstring('rithmschool')) // 7
+// console.log(findLongestSubstring(''))// 0
+// console.log(findLongestSubstring('rithmschool')) // 7
 console.log(findLongestSubstring('thisisawesome')) // 6
 console.log(findLongestSubstring('thecatinthehat')) // 7
 console.log(findLongestSubstring('bbbbbb')) // 1
