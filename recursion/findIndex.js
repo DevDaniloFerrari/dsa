dae3afde6fbc1be0d9d5386e9bc233d07550e5e9
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2025-09-26 06:31:00 -0400

    find index

---
+++ recursion/findIndex.js
@@ -0,0 +1,18 @@
+function findIndex(array, num, start = 0, end = array.length - 1){
+    console.log('interacao')
+    if(start <= end){
+        let mid = Math.floor((start + end) / 2)
+        
+        if(array[mid] === num)
+            return mid
+
+        if(array[mid] > num)
+            return findIndex(array, num, start, mid - 1)
+        else
+            return findIndex(array, num, mid + 1, end)
+    }
+    return -1
+}
+
+
+console.log(findIndex([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 19))
