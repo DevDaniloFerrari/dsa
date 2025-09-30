ab4cea58035f05f3f103c69f1b324f70d9b1857f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2025-09-30 07:28:44 -0300

    bubble sort with comparator

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,12 +1,21 @@
 function bubbleSort(array){
+    let didAnySwap = false
     for(let i = array.length; i > 0; i--){
         for(let j = 0; j < i - 1; j++){
-            if(array[j] > array[j + 1])
+            if(array[j] > array[j + 1]){
                 [array[j], array[j + 1]] = [array[j + 1], array[j]]
+                didAnySwap = true
+            }
         }
+
+        if(!didAnySwap)
+            break
+        else
+            didAnySwap = false
     }
 
     return array
 }
 
-console.log(bubbleSort([4,5,1,7,8,9,3,6,2]))
+// console.log(bubbleSort([4,5,1,7,8,9,3,6,2]))
+console.log(bubbleSort([9,1,2,3,4,5,6,7,8]))
